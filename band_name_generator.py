202c5ab5933b7c7f8afa43b068c77e3df53560f6
Dorothy Rhodes <70044671+dorothy-r@users.noreply.github.com>
2021-02-28 16:48:03 -0500

    Initial commit

---
+++ band_name_generator.py
@@ -0,0 +1,8 @@
+# A greeting
+print("Welcome to the band name generator.")
+# Ask user for home city
+city = input("What city did you grow up in?\n")
+# Ask user for the name of a pet
+pet = input("What was the name of your first pet?\n")
+# Combine city and pet to make band name
+print("Your band name could be " + city + " " + pet)
